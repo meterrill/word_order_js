10616e7ea6da02ef127ba352cdcaa783dd198600
Matthew Terrill <meterrill@gmail.com>
2015-02-12 12:22:44 -0800

    add passing specs to remove and count duplicate words

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
 var wordOrder = function(userPhrase) {
   var wordList = userPhrase.split(" ");
-  return wordList;
+  var sortedArray = wordList.sort();
+
+  var finalWordList = [];
+  var finalWordCount = [];
+
+  for (var i = 0; i < sortedArray.length; i++) {
+    if (sortedArray[i + 1] !== sortedArray[i]) {
+      finalWordList.push(sortedArray[i]);
+    }
+  }
+
+  return finalWordList;
+
+  finalWordList.forEach(function(word) {
+    var count = 0;
+    for (var i = 0; i < sortedArray.length; i++) {
+      if (sortedArray[i] === word) {
+        count ++;
+      }
+    }
+    finalWordCount.push(count);
+  });
+
+  return finalWordCount;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,13 @@
 describe("wordOrder", function() {
   it("will returns a list of words from a string", function() {
-    expect(wordOrder("the dog walks")).to.eql(["the", "dog", "walks"]);
+    expect(wordOrder("the dog walks")).to.eql(["dog", "the", "walks"]);
   });
 
-  it("will not return dulicate words", function() {
-    expect(wordOrder("the dog walks the dog")).to.eql(["the", "dog", "walks"]);
+  it("will not return duplicate words", function() {
+    expect(wordOrder("the dog walks the dog")).to.eql(["dog", "the", "walks"]);
+  });
+
+  it("counts the number of times duplicate words appear", function() {
+    expect(wordOrder("the dog walks the dog dog")).to.eql(["dog", "the", "walks"], [3, 2, 1]);
   });
 });
